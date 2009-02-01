83827710f47b76829a0d2c63cc68a0bb3648928d
jbbarth <jeanbaptiste.barth@gmail.com>
2009-02-01 17:23:17 +0100

    Added an env-home option in server.rb for ruby inline (Permission denied - /root/.ruby_inline)

--- config/server.rb
+++ config/server.rb
@@ -22,6 +22,8 @@
   SL_CONFIG[:DREAMHOST]     = 'no'  # (yes or no) are you deploying this app on a DH server? (see DH_README for details)
 # Database type
   SL_CONFIG[:DB_TYPE_MYSQL] = 'no' # (yes or no) are you using mysql as the database type?
+# Path to make ruby_inline work
+  ENV['HOME'] = '/home/pada/'
 
 # Set your mail configuration for comment notification (optional)
   ActionMailer::Base.smtp_settings = {
