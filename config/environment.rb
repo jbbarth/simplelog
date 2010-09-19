ef08cb5a0964d574b542154cf84e4fd638246510
jbbarth <jeanbaptiste.barth@gmail.com>
2010-09-19 07:48:50 +0200

    Let's try to upgrade to 2.3.9 first

--- config/environment.rb
+++ config/environment.rb
@@ -19,7 +19,7 @@
 # MA 02110-1301 USA.
 #++
 
-RAILS_GEM_VERSION="2.1.2"
+RAILS_GEM_VERSION="2.3.9"
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
