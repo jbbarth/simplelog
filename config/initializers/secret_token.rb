211fcc67bee4018370a8561d6bfa9c2d55b9e329
jbbarth <jeanbaptiste.barth@gmail.com>
2010-09-21 19:37:32 +0200

    Added various files needed by rails 3

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Simplelog::Application.config.secret_token = 'f40dd0e6609293439bf838d51662be1df19872df610672ea4cc159a23f1626f15e9495531a3ba48bb930b3349ee9378af49fbf929c747a8eacb34b134236dab7'
